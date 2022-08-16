c3d0610ca0e3da924377bc491a86c2c6a53a6e22
thanhdat5 <meodc94@gmail.com>
2022-08-16 12:54:22 +0700

    Update UI

--- user/assets/js/script.js
+++ user/assets/js/script.js
@@ -1,10 +1,20 @@
 $(function () {
-    if (!localStorage.getItem('showNotify')) {
-        window.setTimeout(() => {
-            $('.mb-fixed-notice button').trigger('click');
-            localStorage.setItem('showNotify', 'true');
-        }, 3000);
-    }
+    // if (!localStorage.getItem('showNotify')) {
+    //     window.setTimeout(() => {
+    //         $('.mb-fixed-notice button').trigger('click');
+    //         localStorage.setItem('showNotify', 'true');
+    //     }, 3000);
+    // }
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
+
+    $('#btnCreateNew').on('click', function () {
+        $('#SectionNoData').hide();
+        $('#SectionData').show();
+    })
 })
 $(window).scroll(function () {
     var wScroll = $(this).scrollTop();
